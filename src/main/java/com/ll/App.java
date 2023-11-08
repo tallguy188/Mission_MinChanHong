1f58f58e87949313fbbcea8e412e2d1248ed8b82
tallguy188 <cksghd4220@naver.com>
2023-11-08 10:58:30 +0900

    making rq class

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,25 +1,26 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
-    private Scanner sc;
+    static ArrayList<Quote> quotes;
+    static int count;
+    Scanner sc;
 
 
-    public App() {
+
+    App() {
+        quotes = new ArrayList<>();
+        count = 0;
         sc = new Scanner(System.in);
-
     }
 
-    public void run() {
+    void run() {
 
         System.out.println("== 명언 앱 ==");
-
-        QuoteController quoteController = new QuoteController(sc);
-
-
-        while (true) {
+        while(true) {
             System.out.print("명령) ");
 
             String cmd = sc.nextLine();
@@ -28,17 +29,99 @@ public class App {
 
             if (cmd.equals("종료")) {
                 break;
-            } else if (cmd.equals("등록")) {
+            }
+            else if( cmd.equals("등록")) {
 
-                quoteController.actionWrite();
-            } else if (cmd.equals("목록")) {
-                quoteController.actionList();
-            } else if (cmd.startsWith("삭제")) {
-                quoteController.actionDelete(rq);
-            } else if (cmd.startsWith("수정")) {
-                quoteController.actionModify(rq);
+                actionWrite();
+            }
+            else if(cmd.equals("목록")) {
+                actionList();
+            }
+
+            else if (cmd.startsWith("삭제")) {
+                actionDelete(rq);
+            }
+
+            else if (cmd.startsWith("수정")) {
+                actionModify(rq);
             }
         }
+    }
+
+    void actionWrite() {
+
+        count ++;
+        System.out.print("명언 : ");
+        String comment  = sc.nextLine();
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+        Quote quote = new Quote(count,comment,author);
+        quotes.add(quote);
+        System.out.println(quote.getId()+"번 명언이 등록되었습니다.");
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for (int i = quotes.size() - 1; i >= 0; i--) {
+            Quote quote = quotes.get(i);
+            System.out.println(quote.getId() + " / " + quote.getAuthor() + " / " + quote.getComment());
+        }
+
+    }
+
+    void actionDelete(Rq rq){
+
+        boolean found = false;
+
+        int deleteId = rq.getParamAsInt("id",0);
+
+        if (deleteId == 0) {
+            System.out.println("id를 정확하게 입력해주세요.");
+            return;
+        }
+        for (int i = 0; i< quotes.size(); i++) {
+            Quote quote = quotes.get(i);
+            if(quote.getId() == deleteId) {
+                quotes.remove(i);
+                System.out.println(deleteId + "번 명언이 삭제되었습니다.");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found){
+            System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
+        }
+    }
+
+
+    void actionModify(Rq rq) {
+        int modifyId = rq.getParamAsInt("id",0);
+        if(modifyId == 0) {
+            System.out.println("id를 정확하게 입력해주세요.");
+            return;
+        }
+        Quote quote = null;
+        for(Quote q : quotes) {
+            if(q.getId() == modifyId) {
+                quote = q;
+                break;
+            }
+        }
+        String prevComment = quote.getComment();
+        String prevAuthor = quote.getAuthor();
+
+        System.out.println("명언(기존) : " + prevComment);
+        System.out.print("명언 : ");
+        String modiComment = sc.nextLine();
+        System.out.println("작가(기존) : " + prevAuthor);
+        System.out.print("작가 : ");
+        String modiAuthor = sc.nextLine();
+        quote.setComment(modiComment);
+        quote.setAuthor(modiAuthor);
+
+
     }
 
 }
--- src/main/java/com/ll/Quote.java
+++ src/main/java/com/ll/Quote.java
@@ -1,4 +1,20 @@
 package com.ll;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
 public class Quote {
+
+    @Getter
+    private  int id;
+
+    @Getter
+    @Setter
+    private String comment;
+
+    @Getter
+    @Setter
+    private String author;
 }
